820736813c06c05997068079a504847574338bc1
James Kyburz <james.kyburz@gmail.com>
2013-06-24 13:39:07 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+require('attachevent').attachEvent(window, 'orientationchange', function() {
+  var portait  = Math.abs(window.orientation) != 90,
+      viewport = window.document.querySelector('meta[name=viewport]'),
+      content  = viewport.getAttributer('content');
+  content = content.replace(/(width=)(device-[^,]*)/, '$1device-' + portait ? 'width' : 'height');
+  content = content.replace(/(height=)(device-[^,]*)/, '$1device-' + portait ? 'height' : 'width');
+  viewport.setAttribute('content', content);
+});
